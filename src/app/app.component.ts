c0a4c512eb47189b44b45f4c00cbe94ca10a7af6
KevDev0247 <donkevin1980s@gmail.com>
2020-07-30 17:19:06 -0400

    Add app.component logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, Renderer2} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
+import {VERSION as VERSION_ALAIN, TitleService} from '@delon/theme';
+import {VERSION as VERSION_ZORRO, NzModalService} from 'ng-zorro-antd';
+import {filter} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<router-outlet></router-outlet>',
 })
-export class AppComponent {
-  title = 'Jitus-Frontend';
+export class AppComponent implements OnInit {
+  constructor(
+    elementRef: ElementRef,
+    renderer: Renderer2,
+    private router: Router,
+    private titleService: TitleService,
+    private modalService: NzModalService,
+  ) {
+    renderer.setAttribute(elementRef.nativeElement, 'ng-alain-version', VERSION_ALAIN.full);
+    renderer.setAttribute(elementRef.nativeElement, 'ng-zorro-version', VERSION_ZORRO.full)
+  }
+
+  ngOnInit(): void {
+    this.router.events.pipe(filter(evt => evt instanceof NavigationEnd)).subscribe(() => {
+      this.titleService.setTitle();
+      this.modalService.closeAll();
+    });
+  }
 }
