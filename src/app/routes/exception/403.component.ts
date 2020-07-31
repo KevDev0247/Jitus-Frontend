0d01c10445097ec72068241067c72d2a283229b1
KevDev0247 <donkevin1980s@gmail.com>
2020-07-31 13:54:49 -0400

    Eception components initialization

--- src/app/routes/exception/403.component.ts
+++ src/app/routes/exception/403.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import {NzModalService} from 'ng-zorro-antd/ng-zorro-antd.module';
 
 @Component({
   selector: 'exception-403',
   template: ` <exception type="403" style="min-height: 500px; height: 80%;"></exception> `,
 })
-export class Exception403Component {}
+export class Exception403Component {
+  constructor(modalService: NzModalService) {
+    modalService.closeAll();
+  }
+}
--- src/app/routes/exception/404.component.ts
+++ src/app/routes/exception/404.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import {NzModalService} from 'ng-zorro-antd/ng-zorro-antd.module';
 
 @Component({
   selector: 'exception-404',
   template: ` <exception type="404" style="min-height: 500px; height: 80%;"></exception> `,
 })
-export class Exception404Component {}
+export class Exception404Component {
+  constructor(modalService: NzModalService) {
+    modalService.closeAll();
+  }
+}
