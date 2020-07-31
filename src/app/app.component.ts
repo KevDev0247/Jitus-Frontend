cf88f8398dfc86a556906db223df033c357d61c5
YounggangSu <ygsu2001@gmail.com>
2020-07-30 21:50:22 -0400

    New configurations and libraries

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,31 @@
-import {Component, ElementRef, OnInit, Renderer2} from '@angular/core';
-import {NavigationEnd, Router} from '@angular/router';
-import {VERSION as VERSION_ALAIN, TitleService} from '@delon/theme';
-import {VERSION as VERSION_ZORRO, NzModalService} from 'ng-zorro-antd';
-import {filter} from 'rxjs/operators';
+import { Component, OnInit, Renderer2, ElementRef } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { VERSION as VERSION_ALAIN, TitleService } from '@delon/theme';
+import { VERSION as VERSION_ZORRO, NzModalService } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-root',
-  template: '<router-outlet></router-outlet>',
+  template: `
+    <router-outlet></router-outlet>
+  `,
 })
 export class AppComponent implements OnInit {
   constructor(
-    elementRef: ElementRef,
+    el: ElementRef,
     renderer: Renderer2,
     private router: Router,
-    private titleService: TitleService,
-    private modalService: NzModalService,
+    private titleSrv: TitleService,
+    private modalSrv: NzModalService,
   ) {
-    renderer.setAttribute(elementRef.nativeElement, 'ng-alain-version', VERSION_ALAIN.full);
-    renderer.setAttribute(elementRef.nativeElement, 'ng-zorro-version', VERSION_ZORRO.full)
+    renderer.setAttribute(el.nativeElement, 'ng-alain-version', VERSION_ALAIN.full);
+    renderer.setAttribute(el.nativeElement, 'ng-zorro-version', VERSION_ZORRO.full);
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.router.events.pipe(filter(evt => evt instanceof NavigationEnd)).subscribe(() => {
-      this.titleService.setTitle();
-      this.modalService.closeAll();
+      this.titleSrv.setTitle();
+      this.modalSrv.closeAll();
     });
   }
 }
