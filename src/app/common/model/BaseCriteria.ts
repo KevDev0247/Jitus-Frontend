5c4fbf67ca783656bb599b5ba3bcac12647ebfed
KevDev0247 <donkevin1980s@gmail.com>
2020-07-31 12:41:00 -0400

    Base module and criteria

---
+++ src/app/common/model/BaseCriteria.ts
@@ -0,0 +1,5 @@
+export class BaseCriteria {
+  id?: number;
+  skip = 0;
+  page_size = 10;
+}
---
+++ src/app/common/model/BaseModule.ts
@@ -0,0 +1,8 @@
+export class BaseModule {
+  id?: number;
+  creator?: string;
+  updater?: string;
+  createTime?: Date;
+  updateTime?: Date;
+  is_delete?: number;
+}
