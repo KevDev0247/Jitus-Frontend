61166e7c233d21a83d870cd655cccd13ef812238
KevDev0247 <donkevin1980s@gmail.com>
2020-07-31 14:22:55 -0400

    Add UserService in login.component

--- src/app/routes/passport/login/login.component.ts
+++ src/app/routes/passport/login/login.component.ts
@@ -7,6 +7,7 @@ import { DA_SERVICE_TOKEN, ITokenService, SocialOpenType, SocialService } from '
 import { SettingsService, _HttpClient } from '@delon/theme';
 import { environment } from '@env/environment';
 import { NzMessageService } from 'ng-zorro-antd/message';
+import {UserService} from '../../../common/service/UserService';
 
 @Component({
   selector: 'passport-login',
@@ -27,6 +28,7 @@ export class UserLoginComponent implements OnDestroy {
     private startupSrv: StartupService,
     public http: _HttpClient,
     public msg: NzMessageService,
+    private userService: UserService
   ) {
     this.form = fb.group({
       userName: [null, [Validators.required, Validators.pattern(/^(admin|user)$/)]],
@@ -105,12 +107,13 @@ export class UserLoginComponent implements OnDestroy {
 
     // 默认配置中对所有HTTP请求都会强制 [校验](https://ng-alain.com/auth/getting-started) 用户 Token
     // 然一般来说登录请求不需要校验，因此可以在请求URL加上：`/login?_allow_anonymous=true` 表示不触发用户 Token 校验
-    this.http
-      .post('/login/account?_allow_anonymous=true', {
-        type: this.type,
-        userName: this.userName.value,
-        password: this.password.value,
-      })
+    // this.http
+    //   .post('/login/account?_allow_anonymous=true', {
+    //     type: this.type,
+    //     userName: this.userName.value,
+    //     password: this.password.value,
+    //   })
+    this.userService.login(this.userName.value, this.password.value)
       .subscribe((res) => {
         if (res.msg !== 'ok') {
           this.error = res.msg;
