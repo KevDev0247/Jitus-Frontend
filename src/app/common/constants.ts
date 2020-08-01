fdab9210a986e351e5408e7f1de2db1a6b0d384d
KevDev0247 <donkevin1980s@gmail.com>
2020-08-01 15:50:07 -0400

    Change code to original

--- src/app/common/constants.ts
+++ src/app/common/constants.ts
@@ -1,15 +1,14 @@
-export const url = 'http://localhost:8080';
-export const PAGESIZE = 20;
+export const URL = 'http://localhost:8080';
+export const PAGE_SIZE = 20;
+export const IMAGE_SIZE = 5242880;
 
-export const FILE_DOWNLOAD_URL = url + '/resources/upload';
-export const IMAGE_DOWNLOAD_URL = url + '/resources/image';
+export const FILE_DOWNLOAD_URL = URL + '/resource/upload';
+export const IMAGE_DOWNLOAD_URL = URL + '/resource/image';
 
-export const DOMAIN_SERVER_URL = url;
+export const DOMAIN_SERVER_URL = URL;
 export const USER_URL = '/user';
 
 export const UPLOAD_IMAGE_URL = DOMAIN_SERVER_URL + '/file/batch/upload';
 export const UPLOAD_MULTI_URL = DOMAIN_SERVER_URL + '/upload/multiple';
-
-export const IMAGE_SIZE = 5242880; // Size of uploaded image  5M
 
 export const FILE_PATH = 'assets/img';
