93e2282abe364a5328a00797d38cb63353d0134a
YonggangSu <ygsu2001@gmail.com>
2020-08-02 20:02:25 -0400

    Create user-detail components

---
+++ src/app/routes/system/user/user-detail.component.ts
@@ -0,0 +1,58 @@
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
+import {_HttpClient} from '@delon/theme';
+import {NzMessageService} from 'ng-zorro-antd/ng-zorro-antd.module';
+import {User} from '../../../common/model/User';
+
+/**
+ * The service class for user module
+ *
+ * @Author: Yonggang Su
+ */
+@Component({
+  selector: 'app-user-detail',
+  templateUrl: './user-detail.component.html'
+})
+export class UserDetailComponent implements OnInit {
+
+  form: FormGroup;
+  submitting = false;
+  i = null;
+  user: User = new User();
+  id: any;
+
+  constructor(private fb: FormBuilder, private msg: NzMessageService, private cdr: ChangeDetectorRef,
+              private http: _HttpClient, public activatedRoute: ActivatedRoute, private router: Router) {
+
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.id = params.user;
+    });
+    if (this.id) {
+      this.http.get('http://localhost:8080/user/detail', {id: this.id}).subscribe((res: any) => {
+        this.user = res.data;
+      });
+    }
+  }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      name: [null, [Validators.required]],
+      date: [null, [Validators.required]],
+      email: [null, [Validators.required]],
+      password: [null, [Validators.required]],
+    });
+  }
+
+  onChange(result: Date): void {
+    console.log('selected time: ', result);
+  }
+
+  update() {
+    this.http.post('http://localhost:8080/user/detail', this.user).subscribe((res: any) => {
+      if (res.data) {
+        this.router.navigate(['/sys/user/user-list']);
+      }
+    })
+  }
+}
