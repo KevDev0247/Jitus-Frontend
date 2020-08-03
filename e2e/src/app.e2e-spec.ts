eff373b9d79a352d84f69f642272b9ede45c0d45
KevDev0247 <donkevin1980s@gmail.com>
2020-08-03 14:23:35 -0400

    Change application name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Jitus-Frontend app is running!');
+    expect(page.getTitleText()).toEqual('Jitlantis-Frontend app is running!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/Jitus-Frontend'),
+      dir: require('path').join(__dirname, './coverage/Jitlantis-Frontend'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
