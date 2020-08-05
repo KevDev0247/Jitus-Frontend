f73ad7ac8b8e267bdfeeae409eda31161f2744ec
KevDev0247 <donkevin1980s@gmail.com>
2020-08-05 15:56:51 -0400

    Create role.service

---
+++ src/app/common/service/role.service.ts
@@ -0,0 +1,25 @@
+import {Injectable} from '@angular/core';
+import {_HttpClient} from '@delon/theme';
+import {DOMAIN_SERVER_URL} from '../constants';
+
+@Injectable()
+export class RoleService {
+
+  constructor(private http: _HttpClient) { }
+
+  getServiceUrl(): string {
+    return DOMAIN_SERVER_URL + '/role';
+  }
+
+  getList() {
+    return this.http.get(this.getServiceUrl() + '/getRolesList');
+  }
+
+  addMenu(role_id: number, menu_id: number) {
+    return this.http.get(this.getServiceUrl() + '/bindMenu', { roleId: role_id, menuId: menu_id});
+  }
+
+  deleteMenu(role_id: number, menu_id: number) {
+    return this.http.get(this.getServiceUrl() + '/unbindMenu', { roleId: role_id, menuId: menu_id});
+  }
+}
