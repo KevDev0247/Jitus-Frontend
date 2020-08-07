a91ffe141a5d0ef4e6b57e5ac41fe699bb4e1503
KevDev0247 <donkevin1980s@gmail.com>
2020-08-07 13:34:58 -0400

    Create Menu model

---
+++ src/app/common/model/menu.ts
@@ -0,0 +1,10 @@
+import {BaseModule} from './base.module';
+
+export class Menu extends BaseModule {
+  parentId?: number;
+  text?: string;
+  i18n?: string;
+  link?: string;
+  icon?: string;
+  orderNum?: number;
+}
