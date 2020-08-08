fda06f4550772198cfa83797626f45b50cacf7a5
KevDev0247 <donkevin1980s@gmail.com>
2020-08-07 22:12:21 -0400

    Add Menu components into sys.module

--- src/app/routes/system/sys.module.ts
+++ src/app/routes/system/sys.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '@shared';
+import { MenuDetailComponent } from './menu/menu-detail.component';
+import { MenuListComponent } from './menu/menu-list.component';
 import { RoleListComponent } from './role/role-list.component';
 import { RoleComponent } from './role/role.component';
 import { SysRoutingModule } from './sys-routing.module';
@@ -7,12 +9,20 @@ import { UserDetailComponent } from './user/user-detail.component';
 import { UserListComponent } from './user/user-list.component';
 import { UserUpdateComponent } from './user/user-update.component';
 
+/**
+ * The module that define the components and modules in System section.
+ *
+ * @Author Kevin Zhijun Wang, Yonggang Su
+ * @version 2020.0807
+ */
 const COMPONENTS = [
   UserListComponent,
   UserDetailComponent,
   UserUpdateComponent,
   RoleListComponent,
-  RoleComponent
+  RoleComponent,
+  MenuDetailComponent,
+  MenuListComponent,
 ];
 const COMPONENTS_NOROUNT = [];
 
