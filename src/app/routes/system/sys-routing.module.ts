36b9f764bf9a9081174b8e456ec3e57476de41f2
KevDev0247 <donkevin1980s@gmail.com>
2020-08-07 22:09:46 -0400

    Add menu routings in sys routing module

--- src/app/routes/system/sys-routing.module.ts
+++ src/app/routes/system/sys-routing.module.ts
@@ -1,10 +1,18 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {RoleListComponent} from './role/role-list.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MenuDetailComponent } from './menu/menu-detail.component';
+import { MenuListComponent } from './menu/menu-list.component';
+import { RoleListComponent } from './role/role-list.component';
 import { UserDetailComponent } from './user/user-detail.component';
 import { UserListComponent } from './user/user-list.component';
 import { UserUpdateComponent } from './user/user-update.component';
 
+/**
+ * The module that controls the routing of System section.
+ *
+ * @Author Kevin Zhijun Wang, Yonggang Su
+ * @version 2020.0807
+ */
 const routes: Routes = [
   {
     path: 'user',
@@ -17,6 +25,13 @@ const routes: Routes = [
   {
     path: 'role',
     children: [{ path: 'role-list', component: RoleListComponent}],
+  },
+  {
+    path: 'menu',
+    children: [
+      { path: 'menu-list', component: MenuListComponent },
+      { path: 'menu-detail', component: MenuDetailComponent },
+    ],
   }
 ];
 
