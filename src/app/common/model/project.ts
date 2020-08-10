881332f9899c0be7024f19915d75a60a981ac080
KevDev0247 <donkevin1980s@gmail.com>
2020-08-10 12:59:58 -0400

    Create Project model

---
+++ src/app/common/model/project.ts
@@ -0,0 +1,24 @@
+import {BaseModule} from './base.module';
+
+/**
+ * The data model class for Project
+ *
+ * @Author Kevin Zhijun Wang
+ * Created on 2020/08/10
+ */
+export class Project extends BaseModule {
+  name?: string;
+  contractId: number;
+  description?: string;
+  address?: string;
+  clientId?: number;
+  contractStartTime?: Date;
+  contractEndTime?: Date;
+  deliveryTime?: Date;
+  acceptTime?: Date;
+  guaranteeType?: string;
+  guaranteeMonth?: number;
+  guaranteeDueTime?: Date;
+  staffId?: number;
+  fileId: number;
+}
