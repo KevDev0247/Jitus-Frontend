79f6d0145fe4fa78a98a319a5966e7becf486015
YonggangSu <ygsu2001@gmail.com>
2020-08-10 12:44:21 -0400

    Create Client model

---
+++ src/app/common/model/client.ts
@@ -0,0 +1,15 @@
+import {BaseModule} from './base.module';
+
+/**
+ * The data model class for Client
+ *
+ * @Author Yonggang Su
+ * Created on 2020/08/10
+ */
+export class Client extends BaseModule {
+  name?: string;
+  telNo?: string;
+  area?: string;
+  address?: string;
+  remark?: string;
+}
