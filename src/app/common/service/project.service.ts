ba1e62b91cb5316ac2739a15dd041cf3f664ab44
YonggangSu <ygsu2001@gmail.com>
2020-08-11 00:31:51 -0400

    Update methods in project service

--- src/app/common/service/project.service.ts
+++ src/app/common/service/project.service.ts
@@ -1,6 +1,14 @@
+import {Injectable} from '@angular/core';
 import {_HttpClient} from '@delon/theme';
 import {DOMAIN_SERVER_URL} from '../constants';
 
+/**
+ * The service class for project module
+ *
+ * @Author Kevin Zhijun Wang, Yonggang Su
+ * Created on 2020/07/31
+ */
+@Injectable()
 export class ProjectService {
 
   constructor(private http: _HttpClient) { }
@@ -13,11 +21,19 @@ export class ProjectService {
     return this.http.post(this.getServiceUrl() + '/createProject', project);
   }
 
+  update(project: any) {
+    return this.http.post(this.getServiceUrl() + '/updateProject', project);
+  }
+
   delete(userId: number) {
     return this.http.get(this.getServiceUrl() + '/deleteProject', { userId })
   }
 
   getQueryList(contractId?: string, clientId?: string, name?: string) {
     return this.http.get(this.getServiceUrl() + '/queryProjectsList', { contractId, clientId, name });
+  }
+
+  getDetails(id: number) {
+    return this.http.get(this.getServiceUrl() + '/detail?id=' + id)
   }
 }
