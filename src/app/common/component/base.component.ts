a5186f5a10eff768e1c1e010e135f29f158720dc
KevDev0247 <donkevin1980s@gmail.com>
2020-08-13 20:40:38 -0400

    Update base component text

--- src/app/common/component/base.component.ts
+++ src/app/common/component/base.component.ts
@@ -2,25 +2,24 @@ import { NzModalService } from 'ng-zorro-antd';
 
 export abstract class BaseComponent {
 
-    constructor(public modalSrv: NzModalService) {
+    constructor(public modalSrv: NzModalService) { }
 
-    }
     showDeleteConfirm(id: number, content?: string): void {
         if (!content) {
             content = ''
         }
         this.modalSrv.confirm({
-            nzTitle: '你确定要删除?',
+            nzTitle: 'Are you sure to delete?',
             // nzContent: '<b color="red">此操作不可逆 </b>',
-            nzContent: '<b>' + content + '</b> <br/> <font color="red";><b>此操作不可逆</b> </font>',
-            nzOkText: '确定',
+            nzContent: '<b>' + content + '</b> <br/> <font color="red"><b>此操作不可逆</b> </font>',
+            nzOkText: 'OK',
             nzOkType: 'danger',
             nzOnOk: () => {
                 console.log('OK')
                 this.remove(id)
 
             },
-            nzCancelText: '取消',
+            nzCancelText: 'Cancel',
             nzOnCancel: () => console.log('Cancel')
         });
     }
