d07d90312f156b33f84415e52b370fbe68b1cf03
KevDev0247 <donkevin1980s@gmail.com>
2020-08-16 20:16:06 -0400

    Create contact-list component

---
+++ src/app/routes/contact/contact-list.component.ts
@@ -0,0 +1,131 @@
+import {ChangeDetectorRef, OnInit, TemplateRef, ViewChild} from '@angular/core';
+import {Router} from '@angular/router';
+import {STChange, STColumn, STComponent, STData} from '@delon/abc/public_api';
+import {NzMessageService, NzModalService} from 'ng-zorro-antd/ng-zorro-antd.module';
+import {Contact} from '../../common/model/contact';
+import {ContactService} from '../../common/service/contact.service';
+
+export class ContactListComponent implements OnInit {
+
+  query: any = {
+    pi: 0,
+    ps: 10,
+    sorter: '',
+    status: null,
+    statusList: [],
+    param1: '',
+    param2: '',
+    param3: '',
+  };
+
+  contact: Contact = new Contact();
+  data: any[] = [];
+  selectedRows: STData[] = [];
+
+  expandForm = false;
+  loading = false;
+  totalCallNo = 0;
+  total = 0;
+
+  @ViewChild('st', { static: true }) st: STComponent;
+  columns: STColumn[] = [
+    { title: '', index: 'key', type: 'checkbox'},
+    { title: 'Client', index: 'name'},
+    { title: 'Department', index: 'dept'},
+    { title: 'Position', index: 'profession'},
+    { title: 'Phone', index: 'telno'},
+    { title: 'Email', index: 'email'},
+    { title: 'QQ', index: 'qq'},
+    { title: 'WeChat', index: 'wechat'},
+    {
+      title: 'Operations',
+      buttons: [
+        {
+          text: 'View',
+          click: (item: any) => {
+
+          }
+        }
+      ],
+    },
+  ];
+
+  constructor(
+    public messageService: NzMessageService,
+    private modalService: NzModalService,
+    private changeDetectorRef: ChangeDetectorRef,
+    private contactService: ContactService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  getData() {
+    this.loading = true;
+    this.contactService.getQueryList(this.query.param1, this.query.param2, this.query.param3)
+      .subscribe((response: any) => {
+        this.data = response.list;
+        this.loading = false;
+        this.changeDetectorRef.detectChanges();
+      });
+  }
+
+  stChange(event: STChange) {
+    switch (event.type) {
+      case 'checkbox':
+        this.selectedRows = event.checkbox;
+        this.totalCallNo = this.selectedRows.reduce((total, cv) => total + cv.callNo, 0);
+        this.changeDetectorRef.detectChanges();
+        break;
+      case 'filter':
+        this.getData();
+        break;
+    }
+  }
+
+  create() {
+    this.router.navigate(['/contact/detail']);
+  }
+
+  add(tpl: TemplateRef<{}>) {
+    this.modalService.create({
+      nzTitle: '',
+      nzContent: tpl,
+      nzOnOk: () => {
+        this.loading = true;
+        this.contactService.create(this.contact)
+          .subscribe(() => this.getData());
+      },
+    });
+  }
+
+  remove(id: number) {
+    this.contactService.delete(id).subscribe(() => {
+      this.getData();
+      this.st.clearCheck();
+    })
+  }
+
+  reset() {
+    this.query.param1 = '';
+    this.query.param2 = '';
+    this.query.param3 = '';
+    this.getData();
+  }
+
+  showDeleteConfirm(id: number): void {
+    this.modalService.confirm({
+      nzTitle: 'Are you sure to delete?',
+      nzContent: '<b style="color: red;"></b>',
+      nzOkText: 'Sure',
+      nzOkType: 'danger',
+      nzOnOk: () => {
+        console.log('OK');
+        this.remove(id);
+      },
+      nzCancelText: 'Cancel',
+      nzOnCancel: () => console.log('Cancel')
+    });
+  }
+}
