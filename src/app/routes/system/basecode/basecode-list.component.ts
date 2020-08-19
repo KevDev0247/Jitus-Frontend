1e6b3d7b520c6e1c0f3f965d768485a0381e09a9
KevDev0247 <donkevin1980s@gmail.com>
2020-08-18 22:59:44 -0400

    Create basecode-list component

---
+++ src/app/routes/system/basecode/basecode-list.component.ts
@@ -0,0 +1,138 @@
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
+import {Router} from '@angular/router';
+import {STChange, STColumn, STComponent, STData} from '@delon/abc/public_api';
+import {NzMessageService, NzModalService} from 'ng-zorro-antd/ng-zorro-antd.module';
+import {Basecode} from '../../../common/model/basecode';
+import {BasecodeService} from '../../../common/service/basecode.service';
+
+@Component({
+  selector: 'app-basecode-list',
+  templateUrl: './basecode-list.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BasecodeListComponent implements OnInit {
+
+  query: any = {
+    pi: 0,
+    ps: 10,
+    sorter: '',
+    status: null,
+    statusList: [],
+    param1: '',
+    param2: '',
+  };
+
+  basecode: Basecode = new Basecode();
+  data: any[] = [];
+  selectedRow: STData[] = [];
+
+  loading = false;
+  expandForm = false;
+  totalCallNo = 0;
+  total = 0;
+
+  @ViewChild('st', { static: true }) st: STComponent;
+  columns: STColumn[] = [
+    { title: '', index: 'key', type: 'checkbox'},
+    { title: 'Data Type ID', index: 'typeId'},
+    { title: 'Data Type Name', index: 'typename'},
+    { title: 'Data Type ID', index: 'typeId'},
+    { title: 'Field ID', index: 'basecode'},
+    { title: 'Field Name', index: 'basecodename'},
+    {
+      title: 'Operations',
+      buttons: [
+        {
+          text: 'View',
+          click: (item: any) => {
+            this.router.navigate(['/sys/basecode/detail'], { queryParams: { id: item.id } })
+          }
+        },
+        {
+          text: 'Delete',
+          click: (item: any) => {
+            this.showDeleteConfirm(item.id);
+          },
+        },
+      ],
+    },
+  ];
+
+  constructor(
+    public messageService: NzMessageService,
+    private modalService: NzModalService,
+    private changeDetectorRef: ChangeDetectorRef,
+    private basecodeService: BasecodeService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  getData() {
+    this.loading = true;
+    this.basecodeService.getQueryList(this.query.param1, this.query.param2)
+      .subscribe((response: any) => {
+        this.data = response.list;
+        this.loading = false;
+        this.changeDetectorRef.detectChanges();
+      });
+  }
+
+  stChange(event: STChange) {
+    switch (event.type) {
+      case 'checkbox':
+        this.selectedRow = event.checkbox;
+        this.totalCallNo = this.selectedRow.reduce((total, cv) => total + cv.callNo, 0);
+        break;
+      case 'filter':
+        this.getData();
+        break;
+    }
+  }
+
+  add(tpl: TemplateRef<{}>) {
+    this.modalService.create({
+      nzTitle: '',
+      nzContent: tpl,
+      nzOnOk: () => {
+        this.loading = true;
+        this.basecodeService.create(this.basecode)
+          .subscribe(() => this.getData());
+      },
+    });
+  }
+
+  create() {
+    this.router.navigate(['/sys/basecode/detail']);
+  }
+
+  reset() {
+    this.query.param1 = '';
+    this.query.param2 = '';
+    this.getData();
+  }
+
+  remove(id: number) {
+    this.basecodeService.delete(id).subscribe(() => {
+      this.getData();
+      this.st.clearCheck();
+    })
+  }
+
+  showDeleteConfirm(id: number): void {
+    this.modalService.confirm({
+      nzTitle: 'Are you sure to delete?',
+      nzContent: '<b style="color: : red;">This action is irreversible</b>',
+      nzOkText: 'OK',
+      nzOkType: 'danger',
+      nzOnOk: () => {
+        console.log('OK');
+        this.remove(id);
+      },
+      nzCancelText: 'Cancel',
+      nzOnCancel: () => console.log('Cancel')
+    });
+  }
+}
