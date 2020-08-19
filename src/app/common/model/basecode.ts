209db29c955e2334d217fa76081f02ed1c5186b2
YonggangSu <ygsu2001@gmail.com>
2020-08-18 23:00:44 -0400

    Create basecode model

---
+++ src/app/common/model/basecode.ts
@@ -0,0 +1,14 @@
+import { BaseModule } from './base.module';
+
+/**
+ * The data model class for Basecode
+ *
+ * @Author Yonggang Su
+ * Created on 2020/08/18
+ */
+export class Basecode extends BaseModule {
+  typeid?: string;
+  typename?: string;
+  basecode?: string;
+  basecodename?: string;
+}
