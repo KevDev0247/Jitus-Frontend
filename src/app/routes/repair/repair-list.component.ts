54500050a3ce95998e1cf709422150d79f861b61
KevDev0247 <donkevin1980s@gmail.com>
2020-09-08 21:39:55 -0400

    Create repair-list component

---
+++ src/app/routes/repair/repair-list.component.ts
@@ -0,0 +1,142 @@
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
+import { Router } from '@angular/router';
+import {STChange, STColumn, STComponent, STData} from '@delon/abc/public_api';
+import { NzMessageService, NzModalService } from 'ng-zorro-antd/ng-zorro-antd.module';
+import { Repair } from '../../common/model/repair';
+import { RepairService } from '../../common/service/repair.service';
+
+@Component({
+  selector: 'app-repair-list',
+  templateUrl: './repair-list.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class RepairListComponent implements OnInit {
+
+  q: any = {
+    pi: 0,
+    ps: 10,
+    sorter: '',
+    status: null,
+    statusList: [],
+    param1: '',
+    param2: '',
+  };
+
+  repair: Repair = new Repair();
+  selectedRows: STData[] = [];
+  data: any[] = [];
+  expandForm = false;
+  loading = false;
+  totalCallNo = 0;
+
+  @ViewChild('st', { static: true }) st: STComponent;
+  columns: STColumn[] = [
+    { title: '', index: 'key', type: 'checkbox' },
+    { title: 'Agency', index: 'repairUnit' },
+    { title: 'Repair Date', index: 'fixDate' },
+    { title: 'Project', index: 'projectId' },
+    { title: 'Contact', index: 'contactId' },
+    { title: 'Name', index: 'name' },
+    { title: 'Address', index: 'address' },
+    { title: 'Phone', index: 'telno' },
+    { title: 'Product', index: 'installId' },
+    { title: 'Status', index: 'status' },
+    { title: 'Staff', index: 'staffId' },
+    {
+      title: 'Operations',
+      buttons: [
+        {
+          text: 'View',
+          click: (item: any) => {
+            this.router.navigate(['/repair/detail'], { queryParams: { id: item.id } })
+          },
+        },
+        {
+          text: 'Delete',
+          click: (item: any) => {
+            this.showDeleteConfirm(item.id);
+          },
+        },
+      ],
+    },
+  ];
+
+  constructor(
+    public messageService: NzMessageService,
+    private modalService: NzModalService,
+    private changeDetectorRef: ChangeDetectorRef,
+    private repairService: RepairService,
+    private router: Router,
+  ) { }
+
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  getData() {
+    this.loading = true;
+    this.repairService.getQueryList(this.q.param1, this.q.param2)
+      .subscribe((res: any) => {
+        this.data = res.list;
+        this.loading = false
+        this.changeDetectorRef.detectChanges();
+      });
+  }
+
+  stChange(e: STChange) {
+    switch (e.type) {
+      case 'checkbox':
+        this.selectedRows = e.checkbox!;
+        this.totalCallNo = this.selectedRows.reduce((total, cv) => total + cv.callNo, 0);
+        this.changeDetectorRef.detectChanges();
+        break;
+      case 'filter':
+        this.getData();
+        break;
+    }
+  }
+
+  create() {
+    this.router.navigate(['/repair/detail'])
+  }
+
+  add(tpl: TemplateRef<{}>) {
+    this.modalService.create({
+      nzTitle: '',
+      nzContent: tpl,
+      nzOnOk: () => {
+        this.loading = true;
+        this.repairService.create(this.repair)
+          .subscribe(() => this.getData());
+      },
+    });
+  }
+
+  remove(id: number) {
+    this.repairService.delete(id).subscribe(() => {
+      this.getData();
+      this.st.clearCheck();
+    });
+  }
+
+  reset() {
+    this.q.param1 = '';
+    this.q.param2 = '';
+    this.getData()
+  }
+
+  showDeleteConfirm(id: number): void {
+    this.modalService.confirm({
+      nzTitle: 'Are you sure to delete?',
+      nzContent: '<b style="color: red;"></b>',
+      nzOkText: 'OK',
+      nzOkType: 'danger',
+      nzOnOk: () => {
+        console.log('OK');
+        this.remove(id);
+      },
+      nzCancelText: 'Cancel',
+      nzOnCancel: ()  => console.log('Cancel'),
+    });
+  }
+}
