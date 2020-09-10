6e29ed7a001c195f8ba9da91f33caa2dd9508e69
KevDev0247 <donkevin1980s@gmail.com>
2020-09-10 17:54:54 -0400

    Add repair path

--- src/app/routes/repair/repair-routing.module.ts
+++ src/app/routes/repair/repair-routing.module.ts
@@ -2,8 +2,13 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ContractDetailComponent} from './contract/contract-detail.component';
 import {ContractListComponent} from './contract/contract-list.component';
+import {RepairDetailComponent} from './repair-detail.component';
+import {RepairListComponent} from './repair-list.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'list', pathMatch: null },
+  { path: 'list', component: RepairListComponent },
+  { path: 'detail', component: RepairDetailComponent },
   {
     path: 'contract',
     children: [
