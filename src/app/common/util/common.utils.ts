f7025a4210db8cd128161a25e268014287efae91
YonggangSu <ygsu2001@gmail.com>
2020-09-10 01:17:05 -0400

    Create common utilities

---
+++ src/app/common/util/common.utils.ts
@@ -0,0 +1,17 @@
+export class CommonUtils {
+  transFormDateTimeStr(d: Date) {
+    if (d) {
+      return d.getFullYear() +
+        '/' +
+        (d.getMonth() + 1) +
+        '/' +
+        d.getDate() +
+        ' ' +
+        d.getHours() +
+        ':' +
+        d.getMinutes() +
+        ':' +
+        d.getSeconds();
+    }
+  }
+}
