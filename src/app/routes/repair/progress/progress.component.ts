3f05ca1489e513ee8ada72e56584bb9f997ffa6d
KevDev0247 <donkevin1980s@gmail.com>
2020-09-16 01:21:37 -0400

    Create progress component

---
+++ src/app/routes/repair/progress/progress.component.ts
@@ -0,0 +1,66 @@
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
+import {NzMessageService} from 'ng-zorro-antd/ng-zorro-antd.module';
+import {Repair} from '../../../common/model/repair';
+import {RepairService} from '../../../common/service/repair.service';
+import {CommonUtils} from '../../../common/util/common.utils';
+
+@Component({
+  selector: 'app-repair-progress',
+  templateUrl: './progress.component.html',
+})
+export class ProgressComponent implements OnInit {
+
+  form: FormGroup;
+  repair: Repair = new Repair();
+  commonUtils: CommonUtils = new CommonUtils();
+  fixDate: Date;
+
+  id: any;
+  index: any;
+  current = 2;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private messageService: NzMessageService,
+    private changeDetectorRef: ChangeDetectorRef,
+    public activatedRoute: ActivatedRoute,
+    private router: Router,
+    private repairService: RepairService,
+  ) {
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.index = params.status;
+    });
+    if (this.id) {
+      this.repairService.getDetails(this.id).subscribe((res: any) => {
+        this.repair = res.data;
+      });
+    }
+  }
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      repairUnit: [null, []],
+      fixDate: [null, []],
+      projectId: [null, []],
+      contactId: [null, []],
+      name: [null, []],
+      address: [null, []],
+      telno: [null, []],
+      installId: [null, []],
+      status: [null, []],
+      staffId: [null, []],
+      createTime: [null, []],
+      updateTime: [null, []],
+    });
+  }
+
+  goBack() {
+    window.history.back();
+  }
+
+  onIndexChange(event: number): void {
+    this.index = event;
+  }
+}
