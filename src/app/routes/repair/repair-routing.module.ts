6b2598a22d518a5484e5f9cc978bd1603793aada
KevDev0247 <donkevin1980s@gmail.com>
2020-09-16 02:05:54 -0400

    Add url

--- src/app/routes/repair/repair-routing.module.ts
+++ src/app/routes/repair/repair-routing.module.ts
@@ -2,19 +2,26 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ContractDetailComponent} from './contract/contract-detail.component';
 import {ContractListComponent} from './contract/contract-list.component';
+import {ProgressComponent} from './progress/progress.component';
 import {RepairDetailComponent} from './repair-detail.component';
 import {RepairListComponent} from './repair-list.component';
+import {RepairRecordDetailComponent} from './repair-record/repair-record-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'list', pathMatch: null },
   { path: 'list', component: RepairListComponent },
   { path: 'detail', component: RepairDetailComponent },
+  { path: 'progress', component: ProgressComponent },
   {
     path: 'contract',
     children: [
-      { path: 'list', component: ContractListComponent},
+      { path: 'list', component: ContractListComponent },
       { path: 'detail', component: ContractDetailComponent },
     ],
+  },
+  {
+    path: 'record',
+    children: [{ path: 'detail', component: RepairRecordDetailComponent }],
   },
 ];
 
