32f122821f2e83ea9a375cf22ee70ae5544a9127
KevDev0247 <donkevin1980s@gmail.com>
2020-09-16 01:39:41 -0400

    Add new components

--- src/app/routes/repair/repair.module.ts
+++ src/app/routes/repair/repair.module.ts
@@ -3,13 +3,22 @@ import {SharedModule} from '@shared';
 import {NzTableModule} from 'ng-zorro-antd';
 import {ContractDetailComponent} from './contract/contract-detail.component';
 import {ContractListComponent} from './contract/contract-list.component';
+import {ProgressComponent} from './progress/progress.component';
 import {RepairDetailComponent} from './repair-detail.component';
 import {RepairListComponent} from './repair-list.component';
+import {RepairRecordDetailComponent} from './repair-record/repair-record-detail.component';
 import {RepairRoutingModule} from './repair-routing.module';
 
 const COMPONENTS = [];
 
-const COMPONENTS_NOROUNT = [RepairListComponent, RepairDetailComponent, ContractListComponent, ContractDetailComponent];
+const COMPONENTS_NOROUNT = [
+  RepairListComponent,
+  RepairDetailComponent,
+  RepairRecordDetailComponent,
+  ContractListComponent,
+  ContractDetailComponent,
+  ProgressComponent,
+];
 
 @NgModule({
   imports: [
