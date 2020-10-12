be9bd62c802fc6527af9288382a189ae7f3b6c1c
YonggangSu <ygsu2001@gmail.com>
2020-10-12 13:29:04 -0400

    Add in log services

---
+++ src/app/common/service/log.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@angular/core";
+import { DOMAIN_SERVER_URL } from "../constants";
+import { _HttpClient } from '@delon/theme';
+
+@Injectable()
+export class SysLogService {
+  constructor(private http: _HttpClient) {
+
+  }
+
+  getServiceUrl(): string {
+    return DOMAIN_SERVER_URL + '/sysLog';
+  }
+
+  delete(sysLogId: number) {
+    return this.http.get(this.getServiceUrl() + '/deleteLog?sysLogId=' + sysLogId)
+  }
+
+  getQueryList(param1: string, param2: string) {
+    return this.http.get(this.getServiceUrl() + '/queryLogList', { fromTime: param1, toTime: param2 })
+  }
+}
