fd68ab7bae241bd1f2ca4eee67d87020004fff69
KevDev0247 <donkevin1980s@gmail.com>
2020-10-21 23:33:23 -0400

    Create service class for message

---
+++ src/app/common/service/message.service.ts
@@ -0,0 +1,38 @@
+import {Injectable} from '@angular/core';
+import {_HttpClient} from '@delon/theme';
+import {DOMAIN_SERVER_URL} from '../constants';
+
+@Injectable()
+export class MessageService {
+
+  constructor(private http: _HttpClient) { }
+
+  getServiceUrl(): string {
+    return DOMAIN_SERVER_URL + '/sysMessage';
+  }
+
+  create(message: any) {
+    return this.http.post(this.getServiceUrl() + '/createSysMessage');
+  }
+
+  update(message: any) {
+    return this.http.post(this.getServiceUrl() + '/updateSysMessage');
+  }
+
+  delete(messageId: number) {
+    return this.http.get(this.getServiceUrl() + '/deleteSysMessage');
+  }
+
+  detail(messageId: number) {
+    return this.http.get(this.getServiceUrl() + '/detail?id=' + messageId);
+  }
+
+  getQueryList(param1: number, param2: string, param3: number, param4: number) {
+    return this.http.get(this.getServiceUrl() + '/queryList', {
+      type: param1,
+      content: param2,
+      userId: param3,
+      isRead: param4,
+    });
+  }
+}
