a12e275199b49b011f4af002361be07c17989388
KevDev0247 <donkevin1980s@gmail.com>
2020-10-23 00:25:40 -0400

    Update component of repair-list by adding tabs and including status variable

--- src/app/routes/repair/repair-list.component.ts
+++ src/app/routes/repair/repair-list.component.ts
@@ -3,6 +3,7 @@ import {FormBuilder, FormGroup} from '@angular/forms';
 import { Router } from '@angular/router';
 import { STChange, STColumn, STComponent, STData } from '@delon/abc';
 import { NzMessageService, NzModalService } from 'ng-zorro-antd';
+import {NzTabPosition} from 'ng-zorro-antd/ng-zorro-antd.module';
 import { BaseComponent } from '../../common/component/base.component';
 import { Repair } from '../../common/model/repair';
 import { RepairService } from '../../common/service/repair.service';
@@ -23,6 +24,7 @@ export class RepairListComponent extends BaseComponent implements OnInit {
     param1: '',
     param2: '',
     param3: '',
+    param4: '',
   };
 
   repair: Repair = new Repair();
@@ -36,7 +38,7 @@ export class RepairListComponent extends BaseComponent implements OnInit {
   detailIsVisible = false;
   isOkLoading = false;
   isVisible = false;
-  repairId: number = 0;
+  repairId = 0;
   totalCallNo = 0;
   status = 0;
 
@@ -54,7 +56,7 @@ export class RepairListComponent extends BaseComponent implements OnInit {
       title: 'Operations',
       buttons: [
         {
-          text: 'Details',
+          icon: 'Edit',
           click: (item: any) => {
             // this.router.navigate(['/repair/detail'], { queryParams: { id: item.id } });
             this.repairId = item.id;
@@ -72,25 +74,57 @@ export class RepairListComponent extends BaseComponent implements OnInit {
         },
         {
           text: 'Dispatch',
+          iif: item => item.status === 2,
+          iifBehavior: 'disabled',
           click: (item: any) => {
             this.router.navigate(['/repair/record/detail'], { queryParams: { id: item.id, status: item.status } });
           },
         },
         {
-          text: 'Comments',
-          click: (item: any) => {
-            this.router.navigate(['/repair/evaluate'], { queryParams: { id: item.id } })
-          },
-        },
-        {
-          text: 'Delete',
-          click: (item: any) => {
-            this.showDeleteConfirm(item.id);
-          },
+          text: 'More',
+          children: [
+            {
+              text: 'Comment',
+              iif: item => item.status === 6,
+              iifBehavior: 'disabled',
+              click: (item: any) => {
+                this.router.navigate(['/repair/evaluate'], {queryParams: { id: item.id } });
+              },
+            },
+            {
+              text: 'Review',
+              iif: item => item.status === 6,
+              iifBehavior: 'disabled',
+            },
+            {
+              icon: 'Delete',
+              click: (item: any) => {
+                this.showDeleteConfirm(item.id);
+              },
+            },
+          ],
         },
       ],
     },
   ];
+
+  tabs: Array<{ name: number, num: number }> = [
+    { name: 1, num: 5 },
+    { name: 2, num: 6 },
+    { name: 3, num: 7 },
+    { name: 4, num: 8 },
+    { name: 5, num: 9 },
+    { name: 6, num: 10 },
+    { name: 7, num: 11 },
+  ];
+  nzTabPosition: NzTabPosition = 'left';
+  selectedIndex = 0;
+
+  log(args: any[]): void {
+    console.log(args[1].name);
+    this.q.param4 = args[1].name;
+    this.getData();
+  }
 
   constructor(
     public messageService: NzMessageService,
@@ -123,7 +157,8 @@ export class RepairListComponent extends BaseComponent implements OnInit {
 
   getData() {
     this.loading = true;
-    this.repairService.getQueryList(this.q.param1, this.q.param2, this.q.param3)
+    this.repairService
+      .getQueryList(this.q.param1, this.q.param2, this.q.param3, this.q.param4)
       .subscribe((res: any) => {
         this.data = res.list;
         this.loading = false
