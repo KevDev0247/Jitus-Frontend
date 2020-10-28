fbd635736f414f406210b9c37ec4c4ea02a7b214
KevDev0247 <donkevin1980s@gmail.com>
2020-10-27 23:54:11 -0400

    Create service class for attachments module

---
+++ src/app/common/service/attachments.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { DOMAIN_SERVER_URL } from '../constants';
+import { _HttpClient } from '@delon/theme';
+@Injectable()
+export class SysAttachmentsService {
+  constructor(private http: _HttpClient) {}
+
+  getServiceUrl(): string {
+    return DOMAIN_SERVER_URL + '/sysAttachments';
+  }
+
+  create(attachments: any) {
+    return this.http.post(this.getServiceUrl() + '/createSysAttachments', attachments);
+  }
+
+  update(attachments: any) {
+    return this.http.post(this.getServiceUrl() + '/updateSysAttachments', attachments);
+  }
+
+  delete(attachmentsId: number) {
+    return this.http.get(this.getServiceUrl() + '/deleteSysAttachments?attachmentsId=' + attachmentsId);
+  }
+
+  detail(attachmentsId: number) {
+    return this.http.get(this.getServiceUrl() + '/detail?id=' + attachmentsId);
+  }
+  
+  getQueryList(name: string, type: string, userId: number) {
+    return this.http.get(this.getServiceUrl() + '/queryStsAttachmentsList', { name, type, userId });
+  }
+}
