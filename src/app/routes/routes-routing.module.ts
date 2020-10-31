13304ac9e38bbc7349331ee1435c6f6880a8fb8c
YonggangSu <ygsu2001@gmail.com>
2020-10-31 14:06:45 -0400

    Clean up code

--- src/app/routes/routes-routing.module.ts
+++ src/app/routes/routes-routing.module.ts
@@ -73,8 +73,6 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes, {
       useHash: environment.useHash,
-      // NOTICE: If you use `reuse-tab` component and turn on keepingScroll you can set to `disabled`
-      // Pls refer to https://ng-alain.com/components/reuse-tab
       scrollPositionRestoration: 'top',
     }),
   ],
